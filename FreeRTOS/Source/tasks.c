d2442d8e4be4f1f07fac35a24507cf9bedb98bc1
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2021-01-11 15:25:11 -0500

    Remove unused var to remove compile warnings

--- FreeRTOS/Source/tasks.c
+++ FreeRTOS/Source/tasks.c
@@ -2269,7 +2269,6 @@ static void prvAddNewTaskToReadyList( TCB_t *pxNewTCB )
 	void vTaskPreemptionDisable( const TaskHandle_t xTask )
 	{
 	TCB_t *pxTCB;
-	BaseType_t xCoreID;
 
 		taskENTER_CRITICAL();
 		{
